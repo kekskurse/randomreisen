76d7effd5388f95be18694f9ea347209648a7df9
Soeren <soeren@siegismund-poschmann.de>
2014-09-09 11:52:43 +0200

    Add Bootstrap and jquery

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
+<html>
+<head>
+	<title>Random Reisen</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<script src="jquery/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
 <?php
 ini_set("display_errors", 1);
 date_default_timezone_set("Europe/Berlin");
@@ -45,44 +52,95 @@ if(getTextbox("name", "")!="")
 	}
 }
 ?>
-
-<center>
-<h1>Bahnreisen Zufall</h1>
-<form method="GET" name="suchform">
-<b>Startbahnhof:</b><br>
-<input name="name" id="name" <?php echo getTextbox("name", "", true); ?>><br>
-<b>Zugtypen:</b><br>
-<input type="checkbox" name="ice" <?php echo getCheckbox("ice", true, true, "submit"); ?>>ICE 
-<input type="checkbox" name="ic" <?php echo getCheckbox("ic", true, true, "submit"); ?>>IC 
-<input type="checkbox" name="d" <?php echo getCheckbox("d", false, true, "submit"); ?>>D 
-<input type="checkbox" name="nv" <?php echo getCheckbox("nv", false, true, "submit"); ?>>NV<br>
-<b>Reisebeginn:</b><br>
-<input name="stunde" style="width:40px;" <?php echo getTextbox("stunde", date("H"), true); ?>>:<input name="minuten" style="width:40px;" <?php echo getTextbox("minuten", date("i"), true); ?>><br>
-<b>Ankunft in:</b><br>
-<input name="dauer" style="width:40" <?php echo getTextbox("dauer", "60", true); ?>>
-<select name="pm">
-	<option value="5" <?php echo getSelect("pm", "5", "5", true) ?>>+/- 5</option>
-	<option value="10" <?php echo getSelect("pm", "10", "5", true) ?>>+/- 10</option>
-	<option value="15" <?php echo getSelect("pm", "15", "5", true) ?>>+/- 15</option>
-	<option value="20" <?php echo getSelect("pm", "20", "5", true) ?>>+/- 20</option>
-	<option value="60" <?php echo getSelect("pm", "60", "5", true) ?>>+/- 60</option>
-	<option value="120" <?php echo getSelect("pm", "120", "5", true) ?>>+/- 120</option>
-</select> Minuten
-<br>
-<b>Mindes fahrzeit:</b><br>
-<input name="reisedauer" style="width:40" <?php echo getTextbox("reisedauer", "20", true); ?>>Minuten<br>
-<input name="submit" type="submit" value="Suchen">
-</form>
-</center>
+<div class="container-fluid">
+<div class="row" style="margin-top:50px;">
+	<div class="col-md-3"><h1>Random Reisen</h1></div>
+	<div class="col-md-6">
+		<!-- Nav tabs -->
+		<ul class="nav nav-tabs" role="tablist">
+		  <li class="active"><a href="#home" role="tab" data-toggle="tab">Grundsuche</a></li>
+		  <li><a href="#profile" role="tab" data-toggle="tab">Reisedetais</a></li>
+		</ul>
+		<form class="form" id="suchform" name="suchform">
+			<!-- Tab panes -->
+			<div class="tab-content">
+				<div class="tab-pane active" id="home" style="height:150px;">
+					<div class="form-group">
+						<label for="">Startbahnhof</label><br>
+						<div class="col-md-12">
+							<input type="text" style="width:100%" class="form-control" name="name" id="name" <?php echo getTextbox("name", "", true); ?> placeholder="Bahnhof">
+						</div>
+					</div>
+					<div class="row" style="padding-top:40px;">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="">Zugtypen</label>
+								<br>
+								<div class="row">
+									<div class="col-md-3">
+										<input type="checkbox" name="ice" <?php echo getCheckbox("ice", true, true, "submit"); ?>> <img src="http://www.img-bahn.de/v/1131/img/ice_24x24.gif">
+									</div>
+									<div class="col-md-3">
+										<input type="checkbox" name="ic" <?php echo getCheckbox("ic", true, true, "submit"); ?>> <img src="http://www.img-bahn.de/v/1131/img/ec_ic_24x24.gif">
+									</div>
+									<div class="col-md-3">
+										<input type="checkbox" name="d" <?php echo getCheckbox("d", false, true, "submit"); ?>> <img src="http://www.img-bahn.de/v/1131/img/ir_24x24.gif">
+									</div>
+									<div class="col-md-3">
+										<input type="checkbox" name="nv" <?php echo getCheckbox("nv", false, true, "submit"); ?>> <img src="http://www.img-bahn.de/v/1131/img/re_24x24.gif">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="">Reisebeginn</label><br>
+								<input name="stunde" style="width:50px;float:left;" class="form-control" <?php echo getTextbox("stunde", date("H"), true); ?>><span style="float:left;padding-top:5px;">:</span><input name="minuten" class="form-control" style="width:50px;float:left;" <?php echo getTextbox("minuten", date("i"), true); ?>><br>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="tab-pane" id="profile" style="height:150px;">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="">Ankunft in</label><br>
+								<input name="dauer" class="form-control" style="width:60;float:left;" <?php echo getTextbox("dauer", "60", true); ?>>
+								<select name="pm" class="form-control" style="width:110px;float:left;">
+									<option value="5" <?php echo getSelect("pm", "5", "5", true) ?>>+/- 5</option>
+									<option value="10" <?php echo getSelect("pm", "10", "5", true) ?>>+/- 10</option>
+									<option value="15" <?php echo getSelect("pm", "15", "5", true) ?>>+/- 15</option>
+									<option value="20" <?php echo getSelect("pm", "20", "5", true) ?>>+/- 20</option>
+									<option value="60" <?php echo getSelect("pm", "60", "5", true) ?>>+/- 60</option>
+									<option value="120" <?php echo getSelect("pm", "120", "5", true) ?>>+/- 120</option>
+								</select> <span style="float:left;margin-top:8px;"> Minuten</span>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<label for="">Mindest dauer der Bahnfahrt:</label><br>
+							<input name="reisedauer" style="width:60;float:left;" class="form-control" <?php echo getTextbox("reisedauer", "20", true); ?>><span style="float:left;padding-top:8px;"> Minuten</span>
+						</div>
+						
+				</div>
+			</div>
+			<input class="btn btn-default" type="submit" value="Suchen" style="float:right;">
+		</form>
+	</div>
+	<div class="col-md-3"></div>
+</div>
+</div>
 <hr>
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-6">
 <?php
 if(isset($stations)&&count($stations)>1)
 {
-	echo "W&auml;hle deinen Bahnhof und dr&uuml;cke erneut auf 'Suchen'<br>";
+	echo "W&auml;hle deinen Bahnhof und dr&uuml;cke erneut auf 'Suchen'<br><br>";
 	foreach($stations as $station)
 	{
 		?>
-		<a href="#" onclick='document.getElementById("name").value="<?php echo $station["value"]; ?>";document.suchform.submit();'><?php echo $station["value"]; ?></a><br>
+		<div style="width:210px;height:50px;border:1px solid black;float:left;padding:5px;"><a href="#" onclick='document.getElementById("name").value="<?php echo $station["value"]; ?>";document.suchform.submit();'><?php echo $station["value"]; ?></a></div>
 		<?php
 	}
 }
@@ -104,3 +162,7 @@ if(isset($treffer))
 		echo "<center><a href='#' onclick='location.reload();'>Andere Verbindung</a></center>";
 	}
 }
+?>
+	</div>
+	<div class="col-md-3"></div>
+</div>
